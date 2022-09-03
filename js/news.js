58a4ff21e386f00a96b420d66b86ee5e2bc8f7b5
sheikh-momin <momin.emon14@gmail.com>
2022-09-03 07:32:42 -0700

    addn spiner

--- js/news.js
+++ js/news.js
@@ -16,6 +16,7 @@
     });
   }
 const showNews = (category_id )=>{
+  toggleLoading(true)
     fetch(`https://openapi.programming-hero.com/api/news/category/0${category_id}`)
     .then(res =>res.json())
     .then(data => categoriesNews(data.data))
@@ -27,10 +28,11 @@ const categoriesNews = (newsId) => {
   const newsNumber = document.getElementById('news-number')
   newsNumber.value = ''
   newsNumber.value=`${newsId.length} news found `
-  newsId.forEach(news => {
-    const div = document.createElement('div')
-    div.classList.add('card', 'mb-3')
-    div.innerHTML = `<div  class="row g-0">
+  if(newsId.length!==0){
+    newsId.forEach(news => {
+      const div = document.createElement('div')
+      div.classList.add('card', 'mb-3')
+      div.innerHTML = `<div  class="row g-0">
             <div class="col-md-4">
               <img src="${news.image_url ? news.image_url : 'No image found'}" class="img-fluid rounded-start" alt="...">
             </div>
@@ -39,8 +41,8 @@ const categoriesNews = (newsId) => {
                 <h5 class="card-title">${news.title ? news.title : 'No Title Found'}</h5>
                 <p class="card-text">${news.details ? news.details.slice(0, 200) + '...' : 'No Details found'}</p>
                 <div class="d-flex justify-content-between align-items-center">
-                <div><image style="width: 50px;" class="img-fluid rounded-5"  src="${news.author ? news.author.img : 'No image found'}">
-                <h5 class="d-inline-block">${news.author ? news.author.name : 'No name found'}</h5></div>
+                <div><image style="width: 50px;" class="img-fluid rounded-5"  src="${news.author.img ? news.author.img : 'No image found'}">
+                <h5 class="d-inline-block">${news.author.name ? news.author.name : 'No name found'}</h5></div>
                 <P>${news.rating.number ? news.rating.number : 'No rating'}M</P>
                 </div>
                 <button onclick="showDetailsNews('${news._id}')" type="button" class="btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
@@ -49,8 +51,15 @@ const categoriesNews = (newsId) => {
               </div>
             </div>
           </div>`;
-    newsList.appendChild(div)
-  });
+      newsList.appendChild(div)
+
+
+      toggleLoading(false)
+    });
+  }
+  else{
+    alert('No news Found')
+  }
   
   
 }
@@ -60,19 +69,31 @@ const showDetailsNews = (news_id) => {
     .then(data => modalNews(data.data[0]))
 }
 const modalNews = (news_id) => {
-  const modalTitle = document.getElementById('exampleModalLabel');
-  modalTitle.innerText=`${news_id.title}`
-  const modalDetails = document.getElementById('modal-details')
-  modalDetails.innerHTML=`
+    const modalTitle = document.getElementById('exampleModalLabel');
+    modalTitle.innerText = `${news_id.title}`
+    const modalDetails = document.getElementById('modal-details')
+    modalDetails.innerHTML = `
     <img src="${news_id.image_url ? news_id.image_url : 'No image found'}" class="img-fluid rounded-start" alt="...">
     <p class="card-text">${news_id.details ? news_id.details : 'No Details found'}</p>
     <div class="d-flex justify-content-between align-items-center">
-    <div><image style="width: 50px;" class="img-fluid rounded-5"  src="${news_id.author ? news_id.author.img : 'No image found'}">
-    <h5 class="d-inline-block">${news_id.author ? news_id.author.name : 'No name found'}</h5></div>
+    <div><image style="width: 50px;" class="img-fluid rounded-5"  src="${news_id.author.img ? news_id.author.img : 'No image found'}">
+    <h5 class="d-inline-block">${news_id.author.name ? news_id.author.name : 'No name found'}</h5></div>
     <P>${news_id.rating.number ? news_id.rating.number : 'No rating'}M</P>
     </div>
   `
+  
       
+}
+
+// loading
+const toggleLoading = isLoading => {
+  const loader = document.getElementById('loader')
+  if (isLoading === true) {
+    loader.classList.remove('d-none')
+  }
+  else {
+    loader.classList.add('d-none')
+  }
 }
 
 try {
